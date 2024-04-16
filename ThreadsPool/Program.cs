b779174e4c61ce40f4fd848c373ed87d8988f827
EkaterinaDenisova <ekaterina_denisova24@inbox.ru>
2024-04-16 22:15:00 +0900

    added some comments

--- ThreadsPool/Program.cs
+++ ThreadsPool/Program.cs
@@ -5,11 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
 
+// author: Денисова Екатерина
+
 namespace ThreadsPool
 {
     public class Program
     {
-
+        // функция для заполнения матрицы случайными числами
         static void FillMatrixRandom(int[,] matrix, Random rand)
         {
             //Random rand = new Random();
@@ -24,7 +26,11 @@ namespace ThreadsPool
             }
         }
 
-
+        // метод для перемножения матриц
+        // матрицы matrixA и matrixB - исходные матрицы 
+        // результат перемножения записывается в матрицу result
+        // i, j - индекс строки и столбца элемента, значение которого вычисляется
+        // countdownEvent - 
         static void MultiplyCell(object state)
         {
             /*
@@ -37,16 +43,22 @@ namespace ThreadsPool
 
             (int i, int j, int[,] matrixA, int[,] matrixB, int[,] result, CountdownEvent countdownEvent) = ((int, int, int[,], int[,], int[,], CountdownEvent)) state;
 
-            Console.WriteLine($"Поток {Thread.CurrentThread.ManagedThreadId} для элемента [{i}, {j}] запустился");
-            result[i, j] = 0;
+            //Console.WriteLine($"Поток {Thread.CurrentThread.ManagedThreadId} для элемента [{i}, {j}] запустился");
+            //result[i, j] = 0;
+            //object lockObject = new object();
             for (int k = 0; k < matrixA.GetLength(1); k++)
             {
-                result[i, j] += matrixA[i, k] * matrixB[k, j];
+                //lock (lockObject)
+                //{
+                    result[i, j] += matrixA[i, k] * matrixB[k, j];
+                //}
             }
 
             //Thread.Sleep(100); 
-            Console.WriteLine($"Поток {Thread.CurrentThread.ManagedThreadId} для элемента [{i}, {j}] завершился");
+            //Console.WriteLine($"Поток {Thread.CurrentThread.ManagedThreadId} для элемента [{i}, {j}] завершился");
 
+
+            // каждый вызов Signal уменьшает значение счетчика на 1
             countdownEvent.Signal();
         }
 
@@ -54,31 +66,44 @@ namespace ThreadsPool
         static void Main(string[] args)
         {
 
-            int M = 3; // количество строк в матрицах
-            int N = 4; // количество столбцов в матрицах
-            int K = 2; // общее количество столбцов в первой и строк во второй матрицах
+            int M = 1000; // количество строк в матрицах
+            int N = 1000; // количество столбцов в матрицах
+            int K = 1000; // общее количество столбцов в первой и строк во второй матрицах
 
             int[,] matrixA = new int[M, K];
             int[,] matrixB = new int[K, N];
             int[,] result = new int[M, N];
 
+            // примитив синхронизации, снимающий блокировку потока в состоянии ожидания после определенного числа сигналов
             CountdownEvent countdownEvent = new CountdownEvent(matrixA.GetLength(0) * matrixB.GetLength(1));
 
             Random rand = new Random();
             FillMatrixRandom(matrixA, rand);
             FillMatrixRandom(matrixB, rand);
 
+            DateTime start_time = DateTime.Now; // начало измерения времени
 
             for (int i = 0; i < M; i++)
             {
                 for (int j = 0; j < N; j++)
                 {
+                    // QueueUserWorkItem помещает метод MultiplyCell в очередь на выполнение.
+                    // метод выполняется, когда становится доступен поток из пула потоков.
+                    // () - объект, в котором передаются аргументы для метода MultiplyCell
                     ThreadPool.QueueUserWorkItem(MultiplyCell, ( i, j, matrixA, matrixB, result, countdownEvent));
                 }
             }
 
+            // в основном потоке вызов Wait блокируется до тех пор, пока значение счетчика не достигнет нуля
             countdownEvent.Wait();
 
+            DateTime end_time = DateTime.Now; // конец измерения времени
+
+            // время перемножения в миллисекундах
+            int duration = (int)(end_time - start_time).TotalMilliseconds;
+            Console.WriteLine($"Время перемножения матриц в миллисекундах: {duration}");
+
+            /*
             Console.WriteLine("Матрица A:");
             for (int i = 0; i < M; i++)
             {
@@ -108,7 +133,7 @@ namespace ThreadsPool
                     Console.Write(result[i, j] + " ");
                 }
                 Console.WriteLine();
-            }
+            }*/
 
         }
     }
